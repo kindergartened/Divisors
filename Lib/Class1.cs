ae50da09b84c33b676f36babdee237ccdedcdcd6
Pavelavl <sfdrus2004@gmail.com>
2023-09-02 19:50:59 +0300

    Добавьте файлы проекта.

---
+++ Lib/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Lib
+{
+    public class Class1
+    {
+
+        /// <summary>
+        /// Метод, возвращающий лист чисел, имеющих ровно 3 делителя за исключением 1 и самого себя.
+        /// </summary>
+        /// <param name="start">Начальное число</param>
+        /// <param name="finish">Конечное число</param>
+        /// <returns>Лист с числами</returns>
+        static List<int> GetNumsWith3Divisors(int start, int finish)
+        {
+            return new List<int>();
+        }
+    }
+}
