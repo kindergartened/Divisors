8802e5bb66a109e470ecabe911efeb83b1134f01
TheRomaks <niks_2004@bk.ru>
2023-09-04 16:38:19 +0300

    met3,4

--- Divisors/Form1.Designer.cs
+++ Divisors/Form1.Designer.cs
@@ -30,27 +30,33 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.tabPage6 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.label7 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.label8 = new System.Windows.Forms.Label();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
             this.label9 = new System.Windows.Forms.Label();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
             this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -90,6 +96,58 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Method 1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 77);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(549, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Делитель d";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Число n";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(549, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(165, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(315, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IsDivisor";
+            // 
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.Color.LightGray;
@@ -106,97 +164,47 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Method 2";
             // 
-            // tabPage3
+            // button2
             // 
-            this.tabPage3.BackColor = System.Drawing.Color.LightGray;
-            this.tabPage3.Controls.Add(this.label7);
-            this.tabPage3.Location = new System.Drawing.Point(4, 29);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(801, 428);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Method 3";
+            this.button2.Location = new System.Drawing.Point(495, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 71);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Найти";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // tabPage4
+            // label6
             // 
-            this.tabPage4.BackColor = System.Drawing.Color.LightGray;
-            this.tabPage4.Controls.Add(this.label8);
-            this.tabPage4.Location = new System.Drawing.Point(4, 29);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(801, 428);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Method 4";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(300, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Список всех делителей числа";
             // 
-            // tabPage5
+            // richTextBox1
             // 
-            this.tabPage5.BackColor = System.Drawing.Color.LightGray;
-            this.tabPage5.Controls.Add(this.label9);
-            this.tabPage5.Location = new System.Drawing.Point(4, 29);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(801, 428);
-            this.tabPage5.TabIndex = 4;
-            this.tabPage5.Text = "Method 5";
+            this.richTextBox1.Location = new System.Drawing.Point(166, 212);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(478, 165);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
             // 
-            // tabPage6
+            // textBox3
             // 
-            this.tabPage6.BackColor = System.Drawing.Color.LightGray;
-            this.tabPage6.Controls.Add(this.label10);
-            this.tabPage6.Location = new System.Drawing.Point(4, 29);
-            this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(801, 428);
-            this.tabPage6.TabIndex = 5;
-            this.tabPage6.Text = "Method 6";
+            this.textBox3.Location = new System.Drawing.Point(51, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 27);
+            this.textBox3.TabIndex = 2;
             // 
-            // label1
+            // label5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(315, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 38);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "IsDivisor";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(52, 122);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(178, 27);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(549, 122);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(165, 27);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 82);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Число n";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(549, 82);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Делитель d";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(276, 198);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(185, 77);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Проверить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Число n";
             // 
             // label4
             // 
@@ -208,47 +216,17 @@
             this.label4.TabIndex = 0;
             this.label4.Text = " AllDivisors";
             // 
-            // label5
+            // tabPage3
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(76, 97);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 20);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Число n";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(51, 120);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(139, 27);
-            this.textBox3.TabIndex = 2;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(148, 227);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(478, 165);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(300, 189);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(213, 20);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Список всех делителей числа";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(495, 97);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(178, 71);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Найти";
-            this.button2.UseVisualStyleBackColor = true;
+            this.tabPage3.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage3.Controls.Add(this.textBox4);
+            this.tabPage3.Controls.Add(this.label11);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(801, 428);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Method 3";
             // 
             // label7
             // 
@@ -260,6 +238,20 @@
             this.label7.TabIndex = 0;
             this.label7.Text = "IsPrime";
             // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage4.Controls.Add(this.textBox6);
+            this.tabPage4.Controls.Add(this.textBox5);
+            this.tabPage4.Controls.Add(this.label13);
+            this.tabPage4.Controls.Add(this.label12);
+            this.tabPage4.Controls.Add(this.label8);
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(801, 428);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Method 4";
+            // 
             // label8
             // 
             this.label8.AutoSize = true;
@@ -269,6 +261,16 @@
             this.label8.Size = new System.Drawing.Size(187, 38);
             this.label8.TabIndex = 0;
             this.label8.Text = "PrimeInRange";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage5.Controls.Add(this.label9);
+            this.tabPage5.Location = new System.Drawing.Point(4, 29);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(801, 428);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Method 5";
             // 
             // label9
             // 
@@ -280,6 +282,16 @@
             this.label9.TabIndex = 0;
             this.label9.Text = "Factorization";
             // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage6.Controls.Add(this.label10);
+            this.tabPage6.Location = new System.Drawing.Point(4, 29);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(801, 428);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Method 6";
+            // 
             // label10
             // 
             this.label10.AutoSize = true;
@@ -289,6 +301,54 @@
             this.label10.Size = new System.Drawing.Size(298, 38);
             this.label10.TabIndex = 0;
             this.label10.Text = "GetNumsWith3Divisors";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(76, 122);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(64, 20);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Число n";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(41, 155);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(165, 27);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(50, 80);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(127, 20);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "Нижняя граница";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(588, 80);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(129, 20);
+            this.label13.TabIndex = 2;
+            this.label13.Text = "Верхняя граница";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(46, 113);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(592, 113);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(125, 27);
+            this.textBox6.TabIndex = 4;
             // 
             // Form1
             // 
@@ -340,5 +400,11 @@
         private Label label8;
         private Label label9;
         private Label label10;
+        private TextBox textBox4;
+        private Label label11;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private Label label13;
+        private Label label12;
     }
 }
